c276fbc1da6df4638f1b3590a678e43155fa13b7
Jaak Laineste <jaakla@users.noreply.github.com>
2022-03-02 18:24:54 +0200

    dummy

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,5 @@ import * as React from "react";
 import { render } from "react-dom";
 
 import App from "./App";
-
 const rootElement = document.getElementById("root");
 render(<App />, rootElement);
